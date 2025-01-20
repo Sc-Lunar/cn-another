18f2e519c40ba708d74bc9c72be9932c8e634b1d
Sc-Lunar <1656325552@qq.com>
2025-01-21 02:13:06 +0800

    Add files via upload

--- src/app.cjs
+++ src/app.cjs
@@ -29,7 +29,8 @@ const dbPromise = open({
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     downloadCount INTEGER DEFAULT 0,
     voteAverageScore REAL DEFAULT 0,
-    songInfo TEXT
+    songInfo TEXT,
+    from TEXT
   )`);
 
   await db.exec(`CREATE TABLE IF NOT EXISTS votes (
@@ -58,7 +59,6 @@ function transformContent(content) {
   return {
     id: Number(content.id),
     contentType: Number(content.contentType),
-    from: content.from,
     title: content.title,
     publisher: content.publisher,
     description: content.description,
@@ -67,6 +67,7 @@ function transformContent(content) {
     date: new Date(content.date),
     downloadCount: Number(content.downloadCount),
     voteAverageScore: Number(content.voteAverageScore),
+    from: Number(content.from),
     songInfo: JSON.parse(content.songInfo || '{"difficulties":[0,0,0,0,0],"hasLua":false}')
   };
 }
